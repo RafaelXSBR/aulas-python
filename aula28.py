dca7343fd6bc45d52c29a9be99a729e0203667a3
Rafael Xavier Simões <rafael.xsimoes@gmail.com>
2023-02-24 15:26:18 -0300

    atualizando

--- aula28.py
+++ aula28.py
@@ -15,3 +15,11 @@ Se nada for digitado em nome ou idade:
 """
 Nome=input('Digite seu Nome: ')
 idade=input('Digite sua Idade: ')
+if not Nome and  not idade:
+    print ("Desculpe, você deixou campos vazios.")
+else:
+    print (f"Seu nome é {Nome}")
+    print (f"Seu nome invertido é {Nome[::-1]}")
+    print ('Seu nome tem ', str(len(Nome)) ,'letras')
+    print (f"A primeira letra do seu nome é {Nome[0]}")
+    print (f"A última letra do seu nome é {Nome[len(Nome)-1]}")
